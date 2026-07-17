4824d6eee487c97bb3cc507071f88e9ccc627399
grepstrength <admin@grepstrength.dev>
2026-07-17 19:57:52 -0400

    added nat.tf

---
+++ nat.tf
@@ -0,0 +1,29 @@
+resource "azurerm_public_ip" "nat" {
+  name                = "${var.prefix}-nat-pip"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
+
+resource "azurerm_nat_gateway" "main" {
+  name                    = "${var.prefix}-nat"
+  location                = azurerm_resource_group.main.location
+  resource_group_name     = azurerm_resource_group.main.name
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 10
+  tags                    = var.tags
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "main" {
+  nat_gateway_id       = azurerm_nat_gateway.main.id
+  public_ip_address_id = azurerm_public_ip.nat.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "vm" {
+  subnet_id      = azurerm_subnet.vm.id
+  nat_gateway_id = azurerm_nat_gateway.main.id
+}
+
+# we deliberately are not including the Bastion subnet because its a managed service. If we routed it through our NAT, it would break."
